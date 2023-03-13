94fe2ae56ab72c4db3f957278260593b4ce3729e
FilippoFoti <filippofoti7@gmail.com>
2023-03-13 15:59:26 +0100

    Completato esercizio email

--- email/js/script.js
+++ email/js/script.js
@@ -3,7 +3,7 @@
 // // stampa un messaggio appropriato sull’esito del controllo.
 
 // // ALERT
-// alert('Boolean Login')
+// alert('Boolean Login');
 
 // // Chiedere l'email all'utente
 // const arrayEmail = ['filippofoti7@gmail.com', 'alessiolibbi@gmail.com', 'angeloprospero@gmail.com', 'antoniorocco@hotmail.it', 'johndelavega@libero.it', 'hectorperez@gmail.com', 'stefanomartino@hotmail.it'];
@@ -27,14 +27,14 @@
 // VERSIONE IMPUT -------------------------------------------------------------------------------
 
 // // ALERT
-// alert('Boolean Login')
+alert('Boolean Login');
 
 // Chiedere l'email all'utente
 const arrayEmail = ['filippofoti7@gmail.com', 'alessiolibbi@gmail.com', 'angeloprospero@gmail.com', 'antoniorocco@hotmail.it', 'johndelavega@libero.it', 'hectorperez@gmail.com', 'stefanomartino@hotmail.it'];
 
 const loginBtn = document.getElementById("login-btn");
 loginBtn.addEventListener("click", function() {
-    const userEmailInput = document.getElementById("user-email-input");
+    const userEmailInput = document.getElementById("user-email");
     const userEmail = userEmailInput.value;
 
     // Controllo se fa parte della lista
@@ -43,6 +43,35 @@ loginBtn.addEventListener("click", function() {
          if(userEmail === arrayEmail[i]){
              result = 'Email corretta, stai per entrare nel sito.';
          }
-};
+    };
+
+    // Stampo il risultato
+    console.log(arrayEmail);
+    console.log(userEmail);
+    console.log(result);
+
+    document.getElementById("result").innerHTML = `${result}`;
+
+    // Ripulisco i campi di input
+    userEmailInput.value = "";
+
+});
+
+
+// ANNULLA
+
+const cancelBtn = document.getElementById("cancel-btn");
+cancelBtn.addEventListener("click", function() {
+
+    // Prendere il valore del input.
+    const userEmailInput = document.getElementById("user-email");
+    const userEmail = userEmailInput.value;
+
+    console.log(userEmail);
+
+    // Ripulisco i campi di input
+    userEmailInput.value = "";
+
+    document.getElementById("result").innerHTML = '';
 
 });
